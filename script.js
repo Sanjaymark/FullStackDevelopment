e19410c8fe869324952b40ecaac954e04898dcbc
Sanjaymark16 <sanjaymark16@gmail.com>
2023-05-29 16:51:32 +0530

    task

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ async function fetchData()
 {
     try
     {
-        let res = await fetch('https://api.nobelprize.org/2.1/nobelPrizes')
-        let data = await res.json();
-        return data.nobelPrizes;
+        let res = await fetch('https://api.disneyapi.dev/character')
+        let character = await res.json();
+        return character.data;
     }
     catch(error)
     {
@@ -14,43 +14,30 @@ async function fetchData()
 };
   
   
-async function NobelPrizes()
+async function getValues()
 {
-    let Prizes = await fetchData();
+    let data = await fetchData()
 
     let parent = document.createElement('div')
     parent.setAttribute('id','parent')
     document.body.appendChild(parent)
 
-    Prizes.forEach((e)=>
-    {
+    data.forEach((e)=>{
         let child = document.createElement('div')
         child.setAttribute('id','child')
 
         let name = document.createElement('h2')
         name.setAttribute('id','name')
-        name.innerHTML = e.laureates[0].fullName.en
+        name.innerHTML = e.name
         child.appendChild(name)
 
-        let year = document.createElement('h2')
-        year.setAttribute('id','year')
-        year.innerHTML = e.awardYear
-        child.appendChild(year)
-
-        let category = document.createElement('h2')
-        category.setAttribute('id','category')
-        category.innerHTML = e.categoryFullName.en
-        child.appendChild(category)
-
-        let amount = document.createElement('h2')
-        amount.setAttribute('id','amount')
-        amount.innerHTML = e.prizeAmountAdjusted
-        child.appendChild(amount)
-
-        
+        let image = document.createElement('img')
+        image.setAttribute('id','image')
+        image.setAttribute('src',e.imageUrl)
+        child.appendChild(image)
 
         parent.appendChild(child)
     })
 }
 
-NobelPrizes()
+getValues()
